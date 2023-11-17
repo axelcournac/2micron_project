1eb0aa6f27c7d6527a64a285a9d3f31a9a0eede8
axelcournac <acournac@pasteur.fr>
2023-11-17 15:47:55 +0100

    Update create_big_wig.py

--- python_codes/create_big_wig.py
+++ python_codes/create_big_wig.py
@@ -4,11 +4,14 @@
 Created on Mon Jul  5 10:54:10 2021
 @author: axel
 To create a big wig file 
+2: change of normalisation and interpolation 
+to be synchronised with other algos 
 """
 import pyBigWig
 import numpy as np
 import pandas as pd
 import cooler
+from scipy import interpolate
 
 # creation of a bw file: 
 bw = pyBigWig.open("test.bw", "w")
@@ -41,53 +44,89 @@ list_chr =  ["chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8",
              "chr9","chr10","chr11","chr12","chr13","chr14","chr15",
              "chr16"]
 
-## from pairs file
-#file_contact="/media/axel/d0a28364-6c64-4f8e-9efc-f332d9a0f1a91/quiescence_paper/out_SRR7939017_wt_log/tmp/valid_idx_pcrfree.pairs"
-#df=pd.read_table(file_contact,header=None, delimiter="\t", skiprows=21)  # contacts
-#
-#df2 = df.loc[( (df[3] == "plasmid_p2-micron") & (df[1] !=  "plasmid_p2-micron")) ]
-#bin_histo=200
-#for chr1 in list_chr :
-#    print(chr1)
-#    maxi = sizes_dist[chr1]
-#    df_chr= df2.loc[df2[1]==chr1]
-#    v_ip, b_ip = np.histogram(df_chr[2],
-#                              bins=range(0,maxi,bin_histo), 
-#                              density=True)
-#    b_ip = b_ip[:-1].copy()
-#
-#    chroms = np.array([chr1] * len(v_ip) )
-#    starts = np.array(b_ip, dtype=np.int64)
-#    ends = np.array(b_ip+bin_histo, dtype=np.int64)
-#    values0 = np.array(v_ip, dtype=np.float64)
-#
-#    bw.addEntries(chroms, starts, ends=ends, values=values0)
-#    
-#bw.close()
-#
-#
-#bw2 = pyBigWig.open("/home/axel/test.bw")
-#bw2.chroms()
-#bw2.header()
-#
-#
+# interpolation:
+def fill_nan(A):
+    '''
+    interpolate to fill nan values
+    '''
+    inds = np.arange(A.shape[0])
+    good = np.where(np.isfinite(A))
+    f = interpolate.interp1d(inds[good], A[good],bounds_error=False)
+    B = np.where(np.isfinite(A),A,f(inds))
+    return B
+
+# function for nan sum
+def nan_sum(row):
+    total = np.nanmean(row)*len(row[~np.isnan(row)])
+    return total
 
 # from contact file:
+cool_file= "/media/axel/RSG4/diverse_yeast_data/quiescence_2019/fastq/out_Micro-C_WT_log_redone/tmp/valid_idx_pcrfree.pairs.cool"
+#cool_file= "/media/axel/RSG4/diverse_yeast_data/AC/fastq_files/seqs/out3_WT_AC_2micron/tmp/valid_idx_pcrfree.pairs.cool"
+#cool_file= "/media/axel/RSG4/diverse_yeast_data/FG0089_set2/out_FG0089_Set2/tmp/valid_idx_pcrfree.pairs.2000.cool"
 
-cool_file= "/media/axel/RSG4/diverse_yeast_data/quiescence_2019/fastq/out_Micro-C_WT_log_redone/tmp/valid_idx_pcrfree.pairs.cool.200"
+
 c = cooler.Cooler(cool_file)
-cooler.balance_cooler(c, store=True, mad_max=5)   # Normalisation 
-name_bank= "Log_200"
-BIN=200
+cooler.balance_cooler(c, store=True, mad_max=10)   # Normalisation 
+name_bank= "Micro-C_WT_log_redone"
+BIN=2000
+list_chr1 = c.chromnames
 
-chr2="plasmid_p2-micron"
+chr2 = "plasmid_p2-micron"   
+
+total_number_reads=c.info['sum']
+reads_plasmide=0
+for chr3 in list_chr1:
+            m1 = c.matrix(balance=False).fetch(chr2, chr3)
+            if chr2==chr3:
+                reads_plasmide += m1.sum()/2+np.trace(m1)/2
+            else:
+                reads_plasmide += m1.sum()
+
+perc_plasmid=reads_plasmide/total_number_reads
+print("Percentage plasmide")
+print(perc_plasmid)
+
+
 for chr1 in list_chr :
     print(chr1)
-    mat = c.matrix().fetch(chr1, chr2)
-    mat[np.isnan(mat)] = 0 
-    coverage_plasmid = mat.sum(axis=1)
-    coverage_plasmid[coverage_plasmid==0] = np.nan
+    matscn = c.matrix(balance=True).fetch(chr1, chr1)
+    coverage1 = np.apply_along_axis(nan_sum, 0, matscn)
+    mat = c.matrix(balance=True).fetch(chr1, chr2)
+    coverage_plasmid1 = np.apply_along_axis(nan_sum, 1, mat)
+
+    # to work with the raw number of reads 
+    reads_chr1=0
+    coverage=0
+    for chr3 in list_chr1:
+        m1 = c.matrix(balance=False).fetch(chr1, chr3)
+        reads_chr1 += m1.sum()
+        coverage_temp=np.apply_along_axis(nan_sum, 1, m1)
+        coverage_temp[np.isnan(coverage1)] = np.nan
+        coverage += coverage_temp
+
+    m2 = c.matrix(balance=False).fetch(chr2, chr2)
+    reads_chr2= m2.sum()
+    contact_of_plasmid= np.apply_along_axis(nan_sum, 1, m2)
+    m12 = c.matrix(balance=False).fetch(chr1, chr2)
+    reads_chr12= m12.sum()
+    coverage_plasmid = np.apply_along_axis(nan_sum, 1, m12)
+    coverage_plasmid [ np.isnan(coverage_plasmid1)] = np.nan
+
+    coverage_plasmid=coverage_plasmid/coverage # normalisation 
+    coverage_plasmid=coverage_plasmid/perc_plasmid # taking into accournt plasmid presence
     
+    # interpolation:
+
+    if BIN == 2000:
+        proportion_nan = np.count_nonzero(np.isnan(coverage_plasmid))/len(coverage_plasmid)
+        if proportion_nan  < 0.9:
+            coverage_plasmid_new = fill_nan(coverage_plasmid)
+        else :
+            coverage_plasmid_new = np.zeros(len(coverage_plasmid))
+
+    
+    # writting    
     chroms = np.array([chr1] * len(coverage_plasmid) )
     starts = np.arange(0,len(coverage_plasmid)*BIN,BIN)
     ends = starts+BIN
@@ -101,9 +140,6 @@ bw.close()
 
 
 
-
-
-
 #bw = pyBigWig.open("delete.bw", "w")
 #bw.addHeader([("1", 1000)], maxZooms=0)
 #chroms = np.array(["1"] * 10)
@@ -112,18 +148,6 @@ bw.close()
 #values0 = np.array(np.random.random_sample(10), dtype=np.float64)
 #bw.addEntries(chroms, starts, ends=ends, values=values0)
 #bw.close()
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
