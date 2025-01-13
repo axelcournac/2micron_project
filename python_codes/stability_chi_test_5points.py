5e0ba763eeda5e0dab68d1f7005849fe59fb3297
axelcournac <acournac@pasteur.fr>
2025-01-13 17:59:14 +0100

    Update stability_chi_test_5points.py

--- python_codes/stability_chi_test_5points.py
+++ python_codes/stability_chi_test_5points.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 from statsmodels.stats.proportion import proportions_chisquare
+from scipy.stats import chi2_contingency
 import pandas as pd
+
 
 # Représentation du nouveau jeu de données (avec 5 réplicats pour WT et 5toA)
 data = {
@@ -20,8 +22,26 @@ df['souche'] = pd.Categorical(df['souche'], categories=['WT', '5toA'], ordered=T
 
 # Box plot des ratios pour les deux souches
 plt.figure(figsize=(8, 6))
-sns.boxplot(x='souche', y='ratio', data=df, showfliers=False)
-plt.title("Ratios G418/YPD par souche")
+font = {'family' : 'normal',
+        'weight' : 'normal',
+        'size'   : 16}
+
+plt.rc('font', **font)
+
+# sns.boxplot(x='souche', y='ratio', hue='souche',  data=df, showfliers=False)
+
+sns.barplot(x='souche', y='ratio', hue='souche',  
+            data=df,errorbar="sd", capsize=0.1, dodge=False, linewidth=1.5, 
+            edgecolor='black')
+
+plt.xlabel("Strain")
+# plt.bar(x='souche', height='ratio', data=df, yerr=np.std())
+
+# ratio = ratio.reset_index(drop=True)
+sns.stripplot(x='souche', y='ratio', hue='souche', data=df,
+    jitter=True, linewidth=0.5, dodge=False, s=10.0)
+
+# plt.title("Ratios G418/YPD par souche")
 plt.ylabel("Proportion of cells having the plasmid")
 
 # Calcul des p-values (test du chi-deux sur les proportions) entre les souches WT et 5toA
@@ -35,13 +55,18 @@ count = [g418_WT, g418_5toA]
 total = [ypd_WT, ypd_5toA]
 
 # Test du chi-deux sur les proportions
-chi2_stat, p_value, _ = proportions_chisquare(count, total)
+# chi2_stat, p_value, _ = proportions_chisquare(count, total)
+
+# other function
+res= chi2_contingency([count , total])   # allow to have more precise p-values
+chi2_stat, p_value = res.statistic, res.pvalue
 
 # Affichage des résultats de p-value sur le plot
 plt.text(0.5, df['ratio'].max() + 0.05, f'p = {p_value:.2e}', ha='center', fontsize=12)
 
 # Ajustement des limites de l'axe Y pour placer correctement la p-value
 plt.ylim(0, df['ratio'].max() + 0.1)
+plt.legend()
 plt.show()
 
 
